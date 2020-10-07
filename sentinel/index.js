9f0dc4bb1adf9549d7b7ab388c3a49cabacc19ed
ovaphlow <ovaphlow@live.com>
2020-10-07 16:17:12 +0800

    .

--- sentinel/index.js
+++ sentinel/index.js
@@ -1,12 +1,20 @@
 const soap = require('soap');
 
 var url = 'http://ws.webxml.com.cn/WebServices/WeatherWS.asmx?wsdl';
+// http://118.26.65.36:6688/kifsd/service/KmisIfsdInfo?wsdl
+// getGrantKmisInterFaceD(String grantParm)
+// grantParm: 单位编码（附录3.2）+"@"+厂商编码（以传真方式向京天威申请/申请表格在附录3.4？）
+// setInsulationInfo
+// String licenseCode
+// List insulationInfo
 
 soap.createClient(url, (err, client) => {
   if (err) {
     console.error(err);
     return;
   }
+
+  console.info(client);
 
   // client.getRegionDataset({}, (err, result) => {
   //   if (err) {
