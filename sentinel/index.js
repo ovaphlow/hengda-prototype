7e4dfa7a79fdf69a71448cd63c2e08f9a4c2537d
ovaphlow <ovaphlow@live.com>
2020-10-14 09:28:02 +0800

    .

--- sentinel/index.js
+++ sentinel/index.js
@@ -1,54 +1,33 @@
 const soap = require('soap');
 
-var url = 'http://ws.webxml.com.cn/WebServices/WeatherWS.asmx?wsdl';
-// http://118.26.65.36:6688/kifsd/service/KmisIfsdInfo?wsdl
-// getGrantKmisInterFaceD(String grantParm)
-// grantParm: 单位编码（附录3.2）+"@"+厂商编码（以传真方式向京天威申请/申请表格在附录3.4？）
-// setInsulationInfo
-// String licenseCode
-// List insulationInfo
+function getWeather(city) {
+  const url = 'http://ws.webxml.com.cn/WebServices/WeatherWS.asmx?wsdl';
 
-soap.createClient(url, (err, client) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  console.info(client);
-
-  // client.getRegionDataset({}, (err, result) => {
-  //   if (err) {
-  //     console.error(err);
-  //     return;
-  //   }
-  //   console.log(result);
-  //   console.log(result.getRegionDatasetResult);
-  //   console.log(result.getRegionDatasetResult.schema);
-  //   console.log(result.getRegionDatasetResult.diffgram);
-  //   console.log(result.getRegionDatasetResult.diffgram.getRegion.Province);
-  // });
-
-  // client.getRegionProvince({}, (err, result) => {
-  //   if (err) {
-  //     console.error(err);
-  //     return;
-  //   }
-  //   console.info(result.getRegionProvinceResult.string);
-  // });
-
-  // client.getSupportCityString({ theRegionCode: "黑龙江" }, (err, result) => {
-  //   if (err) {
-  //     console.error(err);
-  //     return;
-  //   }
-  //   console.info(result.getSupportCityStringResult.string);
-  // });
-
-  client.getWeather({ theCityCode: '哈尔滨' }, (err, result) => {
+  soap.createClient(url, (err, client) => {
     if (err) {
       console.error(err);
       return;
     }
-    console.info(result.getWeatherResult.string);
+
+    client.getWeather({ theCityCode: city }, (err, result) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.info(result.getWeatherResult.string);
+    });
   });
-});
+}
+
+function unnamed() {
+  // http://118.26.65.36:6688/kifsd/service/KmisIfsdInfo?wsdl
+  // getGrantKmisInterFaceD(String grantParm)
+  // grantParm: 单位编码（附录3.2）+"@"+厂商编码（以传真方式向京天威申请/申请表格在附录3.4？）
+  // setInsulationInfo
+  // String licenseCode
+  // List insulationInfo
+}
+
+if (require.main === module) {
+  getWeather('哈尔滨');
+}
